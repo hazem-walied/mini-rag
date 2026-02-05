26204d15ea7e2a1e2e5668654254f35654893114
Hazem Waleed <hazemwalied2003@gmail.com>
2026-02-05 14:35:47 +0200

    finishing the upload endpoint

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,8 +6,9 @@ class Settings(BaseSettings):
     APP_VERSION: str
     OPENAI_API_KEY: str
 
-    FILE_ALLOWED_EXTENSIONS: list
+    FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
 
     class Config:
         env_file = ".env"
