21331742070c1f6c7f205bbaa3cfeaf8e950eceb
Hazem Waleed <hazemwalied2003@gmail.com>
2026-02-25 02:50:27 +0200

    Done with augmenting answers with prompt templates

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
+from stores.llm.templates.template_parser import TemplateParser
 
 app = FastAPI()
 
@@ -30,7 +31,10 @@ async def startup_span():
     )
     app.vectordb_client.connect()
 
-    
+    app.template_parser = TemplateParser(
+        language=settings.PRIMARY_LANG,
+        default_language=settings.DEFAULT_LANG,
+    )
 
 
 async def shutdown_span():
--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -23,10 +23,11 @@ class OpenAIProvider(LLMInterface):
         self.embedding_size = None
 
         self.client = OpenAI(
-                api_key=self.api_key,
-                base_url=self.api_url
-            )
+            api_key = self.api_key,
+            base_url = self.api_url if self.api_url and len(self.api_url) else None
+        )
 
+        self.enums = OpenAIEnums
         self.logger = logging.getLogger(__name__)
 
     def set_generation_model(self, model_id: str):
@@ -68,7 +69,7 @@ class OpenAIProvider(LLMInterface):
             self.logger.error("Error while generating text with OpenAI")
             return None
 
-        return response.choices[0].message["content"]
+        return response.choices[0].message.content
 
 
     def embed_text(self, text: str, document_type: str = None):
